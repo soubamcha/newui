7c1108a69859348a034f61dcacf6ba50be5d85e8
J Soubamcha <luk4meh@gmail.com>
2018-11-16 21:20:10 +0530

    Added Profile Card

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -2,18 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Avatar from '@material-ui/core/Avatar';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import CardMedia from '@material-ui/core/CardMedia';
+import Divider from '@material-ui/core/Divider';
 
-const styles = {
+const styles = theme => ({
   root: {
     flexGrow: 1,
   },
   appbar: {
     // backgroundColor: '#311b92',
     backgroundColor: 'white',
+  },
+  toolbar: {
+    margin: '0px',
   },
   logo: {
     marginLeft: 'auto',
@@ -23,23 +32,45 @@ const styles = {
     margin: 10,
     cursor: 'pointer',
   },
-};
+  card: {
+    display: 'flex',
+    width: '394px',
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: '1 0 auto',
+  },
+  cover: {
+    width: 151,
+  },
+  controls: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingTop: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+  },
+  button: {
+    margin: theme.spacing.unit,
+  },
+});
 
 class MenuAppBar extends React.Component {
   state = {
-    auth: true,
-    anchorEl: null,
+
   };
 
   render() {
     const { classes } = this.props;
-    const { auth, anchorEl } = this.state;
-    const open = Boolean(anchorEl);
 
     return (
       <div className={classes.root}>
         <AppBar position="static" className={classes.appbar}>
-          <Toolbar>
+          <Toolbar className={classes.toolbar}>
             <IconButton className={classes.menuButton} color="primary" aria-label="Menu">
               <MenuIcon />
             </IconButton>
@@ -49,6 +80,33 @@ class MenuAppBar extends React.Component {
             </div>
           </Toolbar>
         </AppBar>
+        <Card className={classes.card}>
+          <CardMedia
+            className={classes.cover}
+            image="/avatar.jpeg"
+            title="J Soubamcha"
+          />
+          <div className={classes.details}>
+            <CardContent className={classes.content}>
+              <Typography component="h5" variant="h5">
+                J Soubamcha
+              </Typography>
+              <Typography variant="subtitle1" color="textSecondary">
+                Programmer
+              </Typography>
+            </CardContent>
+            <Divider light />
+            <div className={classes.controls}>
+              <Button variant="outlined" color="primary" className={classes.button}>
+                Profile
+              </Button>
+              <Button variant="contained" color="secondary" className={classes.button}>
+                Sign Out
+              </Button>
+            </div>
+          </div>
+          
+        </Card>
       </div>
     );
   }
